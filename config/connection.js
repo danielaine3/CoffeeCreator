75c754c0b42f7b4fb748da24870f0e994be9aac3
Dani Carter <carte470@d.umn.edu>
2018-03-10 17:34:32 -0600

    heroku database update

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-	port:3306,
-  host: "localhost",
-  user: "root",
-	password: "",
-  database: "coffee_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection.mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection.mysql.createConnection({
+    connection = mysql.createConnection({
+    port:3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "coffee_db"
+  });
+};
+
+
 
 connection.connect(function(err) {
   if (err) {
