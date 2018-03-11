fcfde47b2afe9788b1d3ccf0b0d44b8d2c44c471
Dani Carter <carte470@d.umn.edu>
2018-03-10 20:15:32 -0600

    typo in connection

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection.mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection.mysql.createConnection({
+  connection = mysql.createConnection({
     port:3306,
     host: "localhost",
     user: "root",
